3d8ce57beb785f86781f253537950e99d3ac26f6
Tuna AKIN <akint@lafayette.edu>
2026-08-23 19:12:03 -0400

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = self.prev = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {} # key: node
+        self.tail = Node(0, 0)
+        self.head = Node(0, 0)
+        self.tail.prev = self.head
+        self.head.next = self.tail
+
+    def remove(self, node):
+        prv, nxt = node.prev, node.next
+        prv.next = nxt
+        nxt.prev = prv
+
+    def insert(self, node):
+        prv, nxt = self.tail.prev, self.tail
+        prv.next = node
+        node.next = nxt
+        nxt.prev = node
+        node.prev = prv
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self.remove(node)
+        self.insert(node)
+        return node.val        
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        node = Node(key, value)
+        self.insert(node)
+        self.cache[key] = node
+
+        if self.cap < len(self.cache):
+            lru = self.head.next
+            self.remove(lru)
+            del self.cache[lru.key]
